386625dd1f73666b49f77eb7b53ec9dd58c09b4a
squarlhan <squarlhan@264059c0-66d9-11de-8235-5b595ee06194>
2012-12-29 05:11:18 +0000

    初始导入

---
+++ trunk/CSharpSVM/CSharpSVM/LvtongTrain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CSharpSVM
+{
+    class LvtongTrain
+    {
+        public DataSet lvds { get; set; }
+        public List<LvtongTrainData> lvdata { get; set; }
+        public libSVM_Problem Problem { get; set; }
+
+        public LvtongTrain()
+        {
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.绿通数据ConnectionString1);
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            String str = "SELECT distinct " +
+                              " [E01]" +
+                              ",substring(convert(char(10),[E02],108),6,2) as [E02-M]" +
+                              ",substring(convert(char(10),[E02],108),9,2) as [E02-D]" +
+                              ",substring(convert(char(10),[E03],108),1,2) as [E03-H]" +
+                              ",substring(convert(char(10),[E05],108),6,2) as [E05-M]" +
+                              ",substring(convert(char(10),[E05],108),9,2) as [E05-D]" +
+                              ",substring(convert(char(10),[E06],108),1,2) as [E06-H]" +
+                              ",[E07]" +
+                              ",[E09]" +
+                              ",[E10]" +
+                              ",[E11]" +
+                              ",substring([E15],5,2) as [E15-2]" +
+                              ",cast([E04] as float)/100 as [E04]" +
+                              ",datediff(minute,[E03], [E06]) as [E63]" +
+                          "FROM [绿通数据].[dbo].[通关车辆信息表] where E09<=6 and [e11] not like '%,%' and [e11]!='4'";
+            SqlCommand cmd = new SqlCommand(str, con);
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            lvds = new DataSet();
+            adp.Fill(lvds);
+
+            lvdata = new List<LvtongTrainData>();
+            foreach (DataRow mDr in lvds.Tables[0].Rows)
+            {
+                //foreach (DataColumn mDc in lvds.Tables[0].Columns)
+                //{
+                //    Console.Write(mDr[mDc].ToString() + "\t");
+                //}
+                //Console.WriteLine();
+                LvtongTrainData lv = new LvtongTrainData();
+                lv.in_M = int.Parse(mDr[1].ToString());
+                lv.in_D = int.Parse(mDr[2].ToString());
+                lv.in_H = int.Parse(mDr[3].ToString());
+                lv.out_M = int.Parse(mDr[4].ToString());
+                lv.out_D = int.Parse(mDr[5].ToString());
+                lv.out_H = int.Parse(mDr[6].ToString());
+                lv.jianmian = double.Parse(mDr[7].ToString());
+                lv.chezhou = int.Parse(mDr[8].ToString());
+                lv.chezhong = double.Parse(mDr[9].ToString());
+                lv.zaihuo = int.Parse(mDr[10].ToString());
+                lv.bianhao = int.Parse(mDr[11].ToString());
+                lv.ruguan = double.Parse(mDr[12].ToString());
+                lv.shijiancha = int.Parse(mDr[13].ToString());
+                lv.shijiancha = lv.shijiancha < 0 ? lv.shijiancha + 1440 : lv.shijiancha;
+                //Console.WriteLine(lv);
+                lvdata.Add(lv);
+
+            }
+            //Console.WriteLine(lvdata.Count);
+            //Console.ReadLine();
+            con.Close();
+            
+        }
+
+        public void Load_LvData()
+        {
+
+            int nr_samples = lvdata.Count;
+
+            Problem = new libSVM_Problem();
+            Problem.samples = null;
+            Problem.labels = null;
+
+            Problem.samples = new SortedDictionary<int, double>[nr_samples];
+            Problem.labels = new double[nr_samples];
+
+            for(int i = 0;i<=nr_samples-1;i++)
+            {
+
+                SortedDictionary<int, double> sample = new SortedDictionary<int, double>();
+                sample.Add(1, lvdata[i].in_M);
+                sample.Add(2, lvdata[i].in_D);
+                sample.Add(3, lvdata[i].in_H);
+                sample.Add(4, lvdata[i].out_M);
+                sample.Add(5, lvdata[i].out_D);
+                sample.Add(6, lvdata[i].out_H);
+                sample.Add(7, lvdata[i].jianmian);
+                sample.Add(8, lvdata[i].chezhou);
+                sample.Add(9, lvdata[i].chezhong);
+                sample.Add(10, lvdata[i].zaihuo);
+                sample.Add(11, lvdata[i].bianhao);
+                sample.Add(12, lvdata[i].ruguan);
+                sample.Add(13, lvdata[i].shijiancha);
+
+                Problem.samples[i] = sample;
+                Problem.labels[i] = 1;
+            }
+
+        }
+
+        public void doTrian()
+        {
+            libSVM_Parameter Parameter = new libSVM_Parameter();
+            libSVM svm = new libSVM();
+            Parameter.svm_type = SVM_TYPE.ONE_CLASS;
+            Parameter.kernel_type = KERNEL_TYPE.RBF;
+            Parameter.gamma = 0.00003571;
+            Parameter.nu = 0.0381;
+            svm.Train(Problem, Parameter);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ trunk/CSharpSVM/CSharpSVM/LvtongTrainData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpSVM
+{
+    class LvtongTrainData
+    {
+        //车牌号分解
+        public int chepai1 { get; set; }
+        public int chepai2 { get; set; }
+        public int chepai3 { get; set; }
+        public int chepai4 { get; set; }
+        public int chepai5 { get; set; }
+        public int chepai6 { get; set; }
+        public int chepai7 { get; set; }
+        //入关的月日小时
+        public int in_M{ get; set; }
+        public int in_D{ get; set; }
+        public int in_H{ get; set; }
+        //出关的月日小时
+        public int out_M{ get; set; }
+        public int out_D{ get; set; }
+        public int out_H{ get; set; }
+        //减免金额
+        public double jianmian{ get; set; }
+        //车轴数
+        public int chezhou{ get; set; }
+        //车重
+        public double chezhong{ get; set; }
+        //货物类型
+        public int zaihuo{ get; set; }
+        //收费员编号
+        public int bianhao{ get; set; }
+        //入关网点
+        public double ruguan{ get; set; }
+        //运行时间
+        public int shijiancha{ get; set; }
+
+        public LvtongTrainData()
+        {
+             
+        }
+
+        public override string ToString()
+        {
+            double rg = ruguan * 100;
+            return "入关" + in_M + "月" + in_D + "日" + in_H + "点," +
+                "出关" + out_M + "月" + out_D + "日" + out_H + "点," +
+                "可减免" + jianmian + "元," + "车轴数为" + chezhou +
+                "车重为" + chezhong + "吨," + "货物类型为" + zaihuo +
+                ",收费员编号为" + bianhao + ",入关网点为" + rg +
+                ",运行时间为" + shijiancha + "分钟";
+        }
+        
+
+    }
+}
