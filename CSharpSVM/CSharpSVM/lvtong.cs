d74454368428639d2ffc1a3eca929dc8c612c6a6
squarlhan <squarlhan@264059c0-66d9-11de-8235-5b595ee06194>
2012-12-29 05:11:18 +0000

    初始导入

---
+++ CSharpSVM/CSharpSVM/lvtong.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharpSVM {
+    
+    
+    public partial class lvtong {
+    }
+}
+
+namespace CSharpSVM.lvtongTableAdapters {
+    
+    
+    public partial class 通关车辆信息表TableAdapter {
+    }
+}
